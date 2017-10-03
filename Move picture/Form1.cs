246cbec713cab9ba85cd33aacfc19ec4bcb781ed
rc0030810 <rc0030810@CRO-ICF07-23>
2017-10-03 13:29:50 +0100

    Add project files.

---
+++ Move picture/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Move_picture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X + 10, pictureBox2.Location.Y);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X - 10, pictureBox2.Location.Y);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X , pictureBox2.Location.Y - 10);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + 10);
+        }
+    }
+}
